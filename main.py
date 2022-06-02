7e7d9bb816f8a241ff7c38f79075cf01a8289d60
Aditya Sriram Mudragada <59191758+Aditya-commits@users.noreply.github.com>
2022-06-02 13:31:14 +0530

    Add put api, Add book model using pydantic

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Book(BaseModel):
+    title: str
+    price: float
+    author: str
+    is_available: bool
 
 @app.get("/")
 def home():
     return "Welcome to FastAPI !!!"
+
+@app.put("/addBook/{book_id}")
+def add_book(book_id: int, book: Book):
+    return {"book_id": book_id, "title": book.title}
